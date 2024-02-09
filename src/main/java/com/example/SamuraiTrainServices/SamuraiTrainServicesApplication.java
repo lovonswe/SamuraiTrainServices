880be8fd403fdd51397efedf01f9c289f38549fa
nahidswe <nahidswe@gmail.com>
2024-02-09 17:03:19 +0600

    backend structure added.

---
+++ src/main/java/com/example/SamuraiTrainServices/SamuraiTrainServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SamuraiTrainServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamuraiTrainServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamuraiTrainServicesApplication.class, args);
+	}
+
+}
